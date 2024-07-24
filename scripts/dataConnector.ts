b69494ccbdf06a71f7c7dbec239ccdf8cd845da4
AmadiaFlare <anthony.madia@flare.network>
2024-07-24 11:01:46 -0400

    add: getPreparedResponse function

--- scripts/dataConnector.ts
+++ scripts/dataConnector.ts
@@ -83,4 +83,33 @@ async function submitRequest() {
   return roundId;
 }
 
-submitRequest();
+// Obtain response data without proof
+async function getPreparedResponse() {
+  const attestationType = toHex("Payment");
+  const sourceType = toHex("testBTC");
+  // Attestation Request object to be sent to API endpoint
+  const requestData = {
+    attestationType: attestationType,
+    sourceId: sourceType,
+    requestBody: {
+      transactionId: BTC_TRANSACTION_ID,
+      inUtxo: "8",
+      utxo: "4",
+    },
+  };
+
+  const response = await fetch(
+    `${ATTESTATION_URL}/verifier/btc/Payment/prepareResponse`,
+    {
+      method: "POST",
+      headers: {
+        "X-API-KEY": ATTESTATION_API_KEY as string,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    }
+  );
+  const data = await response.json();
+  console.log("Prepared response:", data);
+  return data;
+}
